72d4dcf7414262b9cc77723291d80188649dc88b
Servet DEVECİ <servetdeveci@atlassayac.com>
2021-03-16 11:50:05 +0300

    Add project files.

---
+++ MQTT.Broker/Broker.cs
@@ -0,0 +1,48 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MQTT.Broker
+{
+    public class Broker
+    {
+     
+
+        public void Start()
+        {
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionValidator(c =>
+                {
+                    Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} Endpoint: {c.Endpoint}  ==> ClientId {c.ClientId} username: {c.Username}  ==> password {c.Password}");
+                    c.ReasonCode = MqttConnectReasonCode.Success;
+
+                    //if (c.Username == "atlas" && c.Password == "atlas")
+                    //    c.ReasonCode = MqttConnectReasonCode.Success;
+                    //else
+                    //    c.ReasonCode = MqttConnectReasonCode.NotAuthorized;
+                })
+                .WithApplicationMessageInterceptor(async context =>
+                {
+                    // await SendToApi(context); // apiye gönderme 
+                })
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointBoundIPAddress(System.Net.IPAddress.Parse("192.168.1.101"))
+                .WithDefaultEndpointPort(1884);
+
+            //start server
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            mqttServer.StartAsync(optionsBuilder.Build()).Wait();
+
+            Console.WriteLine($"Mqtt Broker çalışıyor: Host: {mqttServer.Options.DefaultEndpointOptions.BoundInterNetworkAddress} Port: {mqttServer.Options.DefaultEndpointOptions.Port}");
+
+            Console.ReadLine();
+            Task.Run(() => Thread.Sleep(Timeout.Infinite)).Wait();
+            mqttServer.StopAsync().Wait();
+        }
+    }
+}
---
+++ MQTT.Broker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQTT.Broker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
